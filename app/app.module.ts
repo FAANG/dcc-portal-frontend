0d5d416a8421c615ff3589cac48487a636fba089
Peter Harrison <peter@ebi.ac.uk>
2016-12-15 09:29:37 +0000

    Add skeleton shared module

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,11 +5,12 @@ import { AppComponent }  from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { CoreModule }  from './core/core.module';
+import { SharedModule }  from './shared/shared.module';
 import { SpecimenModule } from './specimen/specimen.module';
 import { OrganismModule } from './organism/organism.module';
 
 @NgModule({
-  imports: [ BrowserModule, CoreModule, AppRoutingModule, SpecimenModule, OrganismModule ],
+  imports: [ BrowserModule, CoreModule, AppRoutingModule, SharedModule, SpecimenModule, OrganismModule ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
--- app/specimen/specimen.module.ts
+++ app/specimen/specimen.module.ts
@@ -1,10 +1,12 @@
 import { NgModule }      from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
 
 import { SpecimenTableComponent }  from './specimen-table.component';
 import { SpecimenDetailComponent }  from './specimen-detail.component';
 import { SpecimenFilesComponent }  from './specimen-files.component';
 
 @NgModule({
+  imports: [ SharedModule ],
   declarations: [ 
     SpecimenTableComponent,
     SpecimenDetailComponent,
