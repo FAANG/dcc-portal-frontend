0379b70a015aaf13e021486e46176958ac68c6a7
streeter <streeter@ebi.ac.uk>
2016-12-21 16:43:06 +0000

    Navigation bar

--- app/shared/shared.module.ts
+++ app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { RouterModule } from '@angular/router';
 
 import { ApiErrorComponent } from './components/api-error.component';
 import { ApiSlowResponseComponent } from './components/api-slow-response.component';
+import { NavbarComponent } from './components/navbar.component';
 
 @NgModule({
   imports: [ CommonModule, RouterModule ],
-  declarations: [ ApiErrorComponent, ApiSlowResponseComponent ],
-  exports: [ CommonModule, RouterModule, ApiErrorComponent, ApiSlowResponseComponent ]
+  declarations: [ ApiErrorComponent, ApiSlowResponseComponent, NavbarComponent ],
+  exports: [ CommonModule, RouterModule, ApiErrorComponent, ApiSlowResponseComponent, NavbarComponent ]
 })
 export class SharedModule { }
