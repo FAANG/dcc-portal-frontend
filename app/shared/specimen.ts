a6177c5502e917ae0c9af4f9bfbc8ee517745c95
Peter Harrison <peter@ebi.ac.uk>
2016-12-21 10:53:40 +0000

    Add specimen detail

--- app/shared/specimen.ts
+++ app/shared/specimen.ts
@@ -7,16 +7,17 @@ export class Specimen {
     readonly project: string,
     readonly organization: {
       readonly name: string,
-      readonly role: string,
+      readonly URL: string,
     }[],
     readonly material: {
       readonly text: string,
       readonly ontologyTerms: string,
     },
-    readonly derivedFrom: string[],
+    readonly derivedFrom: string,
     readonly sameAs: string[],
-    readonly availability: string[],
+    readonly availability: string,
     readonly organism: {
+      readonly biosampleId: string,
       readonly organism: {
         readonly text: string,
         readonly ontologyTerms: string,
@@ -61,6 +62,78 @@ export class Specimen {
         readonly text: number,
         readonly unit: string,
       },
+      readonly specimenVolume: {
+        readonly text: number,
+        readonly unit: string,
+      },
+      readonly specimenSize: {
+        readonly text: number,
+        readonly unit: string,
+      },
+      readonly specimenWeight: {
+        readonly text: number,
+        readonly unit: string,
+      },
+      readonly specimenPictureUrl: string[],
+      readonly gestationalAgeAtSampleCollection: {
+        readonly text: number,
+        readonly unit: string,
+      }
+    },
+    readonly cellSpecimen: {
+      readonly markers: string,
+      readonly cellType: {
+        readonly text: string,
+        readonly ontologyTerms: string,
+      }[],
+      readonly purificationProtocol: string
+    },
+    readonly cellCulture: {
+      readonly cultureType: {
+        readonly text: string,
+        readonly ontologyTerms: string,
+      },
+      readonly cellType: {
+        readonly text: string,
+        readonly ontologyTerms: string,
+      },
+      readonly cellCultureProtocol: string,
+      readonly cultureConditions: string,
+      readonly numberOfPassages: number
+    },
+    readonly cellLine: {
+      readonly organism: {
+        readonly text: string,
+        readonly ontologyTerms: string,
+      },
+      readonly sex: {
+        readonly text: string,
+        readonly ontologyTerms: string,
+      },
+      readonly cellLine: string,
+      readonly biomaterialProvider: string,
+      readonly catalogueNumber: string,
+      readonly passageNumber: number,
+      readonly dateEstablished: {
+        readonly text: number,
+        readonly unit: string,
+      },
+      readonly publication: string,
+      readonly breed: {
+        readonly text: string,
+        readonly ontologyTerms: string,
+      },
+      readonly cellType: {
+        readonly text: string,
+        readonly ontologyTerms: string,
+      },
+      readonly cultureConditions: string,
+      readonly cultureProtocol: string,
+      readonly disease: {
+        readonly text: string,
+        readonly ontologyTerms: string,
+      },
+      readonly karyotype: string,
     },
   ) { }
 }
