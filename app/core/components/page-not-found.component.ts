8ebad16ff7a173f46e70c80aa4e4718c0e22dacb
streeter <streeter@ebi.ac.uk>
2016-12-22 11:31:20 +0000

    Page Not Found component

---
+++ app/core/components/page-not-found.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute } from'@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+
+@Component({
+    templateUrl: './page-not-found.component.html',
+})
+export class PageNotFoundComponent implements OnInit, OnDestroy {
+
+  // public properties
+  currentUrl: string;
+
+  // private properties
+  private routeSubscription: Subscription = null;
+
+  public constructor(
+    private titleService: Title,
+    private activatedRoute: ActivatedRoute,
+  ) { }
+
+  ngOnInit() {
+    this.titleService.setTitle('FAANG | whoops');
+    this.routeSubscription =
+      this.activatedRoute.url.subscribe(segments => this.currentUrl = segments.join(''));
+  }
+
+  ngOnDestroy() {
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
+    }
+  }
+};
