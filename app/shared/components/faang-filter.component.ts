536c0e9583721cc4a05b699655a5a441cb554f03
Peter Harrison <peter@ebi.ac.uk>
2017-01-09 17:11:23 +0000

    Singular clickable filters

--- app/shared/components/faang-filter.component.ts
+++ app/shared/components/faang-filter.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute, Router } from'@angular/router';
 @Component({
     selector: 'faang-filter',
     templateUrl: './faang-filter.component.html',
@@ -10,6 +11,33 @@ import { Component, Input } from '@angular/core';
 })
 export class FaangFilterComponent{
   @Input() title: string;
+  @Input() routekey: string;
   @Input() aggs: {key: string, doc_count: number}[];
   @Input() isFiltered: {[key: string] : boolean};
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ){ };
+  toggleFilter(key: string){
+    console.log("I am the key", key)
+    console.log("Filtered", this.isFiltered)
+    this.isFiltered[key] = !this.isFiltered[key];
+    console.log("Reversed filtered",this.isFiltered)
+    let oldParams = this.activatedRoute.snapshot.queryParams;
+    console.log("Existing params", oldParams)
+    let newParams = {}
+    for (let filter in oldParams){
+      newParams[filter] = oldParams[filter]
+    }
+    newParams[this.routekey] = []
+    console.log("Wiped params", newParams)
+    for (let filter in this.isFiltered){
+      if (this.isFiltered[filter]){
+        newParams[this.routekey].push(filter)
+      }
+    }
+    console.log("Edited Params", newParams)
+    this.router.navigate([], {relativeTo:this.activatedRoute, queryParams: newParams})
+  }
 }
