6cf84b9425b1e728d8a96a51088f15b469c466b5
Jun Fan <junf@ebi.ac.uk>
2017-07-14 15:33:56 +0100

    replace <a> element with <robust-link>

---
+++ src/app/shared/robust-link.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'robust-link',
+    templateUrl: './robust-link.component.html',
+})
+export class RobustLinkComponent{ 
+//	public properties
+  @Input() text: string;
+  @Input() link: string;
+  constructor() {
+  }
+};
