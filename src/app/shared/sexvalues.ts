00301614da29ff20263e50e36a747bc82452fdde
Alexey Sokolov <alexey@ebi.ac.uk>
2019-03-11 12:41:19 +0000

    Fixing bug with sex

--- src/app/shared/sexvalues.ts
+++ src/app/shared/sexvalues.ts
@@ -1,2 +1,2 @@
-export const male_values = ['male', 'male genotypic sex', 'intact male', 'M'];
-export const female_values = ['female', 'female genotypic sex', 'intact female', 'F'];
+export const male_values = ['male', 'male genotypic sex', 'intact male', 'M', 'Male'];
+export const female_values = ['female', 'female genotypic sex', 'intact female', 'F', 'Female'];
