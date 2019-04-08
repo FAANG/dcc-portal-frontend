a688c5b93d30174a9eca62f833e243745c03d0d4
Alexey Sokolov <alexey@ebi.ac.uk>
2019-04-08 09:51:16 +0100

    Fixing bug with header active class

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,12 +9,26 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   collapsed = true;
 
-  ngOnInit() {
+  constructor(private router: Router) {}
 
+  ngOnInit() {
   }
 
   toggleCollapse() {
     this.collapsed = ! this.collapsed;
+  }
+
+  isActiveSummary() {
+    if (this.router.url === '/summary/organisms' || this.router.url === '/summary/specimens' || this.router.url === '/summary/datasets' ||
+      this.router.url === '/summary/files') {
+      return 'active';
+    }
+  }
+
+  isActiveProtocols() {
+    if (this.router.url === '/protocol/samples' || this.router.url === '/protocol/experiments') {
+      return 'active';
+    }
   }
 
 }
