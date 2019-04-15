bffbef05acfeb3394ae03395cde92a2adae25e54
Alexey Sokolov <alexey@ebi.ac.uk>
2019-04-15 16:07:05 +0100

    Adding test on header content and fixing bugs related to that

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,58 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display welcome message on home page', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to faang-portal-frontend-angular6-second!');
+    expect(page.getParagraphText()).toEqual('Data Portal');
+  });
+
+  it('should display FAANG files on file path', () => {
+    page.navigateToFile();
+    expect(page.getParagraphTextForPages()).toEqual('FAANG files');
+  });
+
+  it('should display FAANG organisms on organism path', () => {
+    page.navigateToOrganism();
+    expect(page.getParagraphTextForPages()).toEqual('FAANG organisms');
+  });
+
+  it('should display FAANG specimens on specimens path', () => {
+    page.navigateToSpecimen();
+    expect(page.getParagraphTextForPages()).toEqual('FAANG specimens');
+  });
+
+  it('should display FAANG datasets on dataset path', () => {
+    page.navigateToDatset();
+    expect(page.getParagraphTextForPages()).toEqual('FAANG datasets');
+  });
+
+  it('should display FAANG protocols on protocol/samples path', () => {
+    page.navigateToProtocolSamples();
+    expect(page.getParagraphTextForPages()).toContain('FAANG protocols');
+  });
+
+  it('should display FAANG protocols on protocol/experiments path', () => {
+    page.navigateToProtocolExperiments();
+    expect(page.getParagraphTextForPages()).toContain('FAANG protocols');
+  });
+
+  it('should display FAANG Summary on summary/organisms path', () => {
+    page.navigateToSummaryOrganisms();
+    expect(page.getParagraphTextForPages()).toContain('FAANG Summary');
+  });
+
+  it('should display FAANG Summary on summary/specimens path', () => {
+    page.navigateToSummarySpecimens();
+    expect(page.getParagraphTextForPages()).toContain('FAANG Summary');
+  });
+
+  it('should display FAANG Summary on summary/datasets path', () => {
+    page.navigateToSummaryDatasets();
+    expect(page.getParagraphTextForPages()).toContain('FAANG Summary');
+  });
+
+  it('should display FAANG Summary on summary/files path', () => {
+    page.navigateToSummaryFiles();
+    expect(page.getParagraphTextForPages()).toContain('FAANG Summary');
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -5,7 +5,51 @@ export class AppPage {
     return browser.get('/');
   }
 
+  navigateToFile() {
+    return browser.get('/file');
+  }
+
+  navigateToOrganism() {
+    return browser.get('/organism');
+  }
+
+  navigateToSpecimen() {
+    return browser.get('/specimen');
+  }
+
+  navigateToDatset() {
+    return browser.get('/dataset');
+  }
+
+  navigateToProtocolSamples() {
+    return browser.get('/protocol/samples');
+  }
+
+  navigateToProtocolExperiments() {
+    return browser.get('/protocol/experiments');
+  }
+
+  navigateToSummaryOrganisms() {
+    return browser.get('/summary/organisms');
+  }
+
+  navigateToSummarySpecimens() {
+    return browser.get('/summary/specimens');
+  }
+
+  navigateToSummaryDatasets() {
+    return browser.get('/summary/datasets');
+  }
+
+  navigateToSummaryFiles() {
+    return browser.get('/summary/files');
+  }
+
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
+  }
+
+  getParagraphTextForPages() {
+    return element(by.css('h2')).getText();
   }
 }
