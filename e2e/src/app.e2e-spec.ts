50aecd410b60910cd9474d00b1368f6c609b5268
Alexey Sokolov <alexey@ebi.ac.uk>
2019-04-16 12:40:40 +0100

    Adding test for organisms page

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,32 +1,54 @@
 import { AppPage } from './app.po';
-import {by, element} from 'protractor';
+import {by, element, browser} from 'protractor';
 
-describe('Test home page', () => {
-  let page: AppPage;
-
-  beforeEach(() => {
-    page = new AppPage();
-  });
-
-  it('should display welcome message on home page', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Data Portal');
-  });
-});
-//
-// describe('Test organisms page', () => {
+// describe('Test home page', () => {
 //   let page: AppPage;
 //
 //   beforeEach(() => {
 //     page = new AppPage();
 //   });
 //
-//   it('should display FAANG organisms on organism path', () => {
-//     page.navigateToOrganism();
-//     expect(page.getParagraphTextForPages()).toEqual('FAANG organisms');
+//   it('should display welcome message on home page', () => {
+//     page.navigateTo();
+//     expect(page.getParagraphText()).toEqual('Data Portal');
 //   });
 // });
-//
+
+describe('Test organisms page', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+    page.navigateToOrganism();
+  });
+
+  it('should display FAANG organisms on organism path', () => {
+
+    expect(page.getParagraphTextForPages()).toEqual('FAANG organisms');
+  });
+
+  it('should sort table on organism path', () => {
+    expect(element(by.css('tbody')).all(by.css('tr')).first().all(by.css('td')).get(2).getText()).toEqual('Equus caballus');
+    element.all(by.css('th')).get(2).click().then(function() {
+      browser.sleep(1000);
+      expect(element(by.css('tbody')).all(by.css('tr')).first().all(by.css('td')).get(2).getText()).toEqual('Sus scrofa');
+      element.all(by.css('th')).get(2).click().then(function() {
+        browser.sleep(1000);
+        expect(element(by.css('tbody')).all(by.css('tr')).first().all(by.css('td')).get(2).getText()).toEqual('Bos indicus');
+      });
+    });
+  });
+
+  it('should filter table on organism path', () => {
+    const before = element.all(by.css('.list-group-item')).first().all(by.css('span')).first().getText();
+    element.all(by.css('.list-group-item')).get(1).click().then(function() {
+      browser.sleep(1000);
+      const after = element.all(by.css('.list-group-item')).first().all(by.css('span')).first().getText();
+      expect(before).toBeGreaterThan(after);
+    });
+  });
+});
+
 // describe('Test specimens page', () => {
 //   let page: AppPage;
 //
@@ -144,56 +166,56 @@ describe('Test home page', () => {
 //   });
 // });
 
-describe('Test search page', () => {
-  let page: AppPage;
+// describe('Test search page', () => {
+//   let page: AppPage;
+//
+//   beforeEach(() => {
+//     page = new AppPage();
+//     page.navigateToSearch();
+//   });
+//
+//   it('should display Search FAANG placeholder on search path', () => {
+//     expect(element(by.css('.form-control')).getAttribute('placeholder')).toEqual('Search FAANG');
+//   });
+//
+//   it('should have checkbox that will exclude legacy data', () => {
+//     expect(element(by.css('.checkbox')).element(by.tagName('label')).getText()).toEqual('Show only FAANG data (exclude legacy data)');
+//   });
+//
+//   it('should display 78 matching datasets on a search path when search for sus scrofa', () => {
+//     element(by.css('.form-control')).sendKeys('sus scrofa').then(function () {
+//       expect(element.all(by.css('h4')).last().getText()).toEqual('78 matching datasets');
+//     });
+//   });
+//
+//   it('should display 11 matching datasets on a search path when search for sus scrofa with legacy data excluded', () => {
+//     element(by.css('.form-control')).sendKeys('sus scrofa').then(function () {
+//       element(by.css('.checkbox')).element(by.tagName('label')).element(by.tagName('input')).click().then(function() {
+//         expect(element.all(by.css('h4')).last().getText()).toEqual('11 matching datasets');
+//       });
+//     });
+//   });
+// });
 
-  beforeEach(() => {
-    page = new AppPage();
-    page.navigateToSearch();
-  });
-
-  it('should display Search FAANG placeholder on search path', () => {
-    expect(element(by.css('.form-control')).getAttribute('placeholder')).toEqual('Search FAANG');
-  });
-
-  it('should have checkbox that will exclude legacy data', () => {
-    expect(element(by.css('.checkbox')).element(by.tagName('label')).getText()).toEqual('Show only FAANG data (exclude legacy data)');
-  });
-
-  it('should display 78 matching datasets on a search path when search for sus scrofa', () => {
-    element(by.css('.form-control')).sendKeys('sus scrofa').then(function () {
-      expect(element.all(by.css('h4')).last().getText()).toEqual('78 matching datasets');
-    });
-  });
-
-  it('should display 11 matching datasets on a search path when search for sus scrofa with legacy data excluded', () => {
-    element(by.css('.form-control')).sendKeys('sus scrofa').then(function () {
-      element(by.css('.checkbox')).element(by.tagName('label')).element(by.tagName('input')).click().then(function() {
-        expect(element.all(by.css('h4')).last().getText()).toEqual('11 matching datasets');
-      });
-    });
-  });
-});
-
-describe('Test help page', () => {
-  let page: AppPage;
-
-  beforeEach(() => {
-    page = new AppPage();
-    page.navigateToHelp();
-  });
-
-  it('should display Frequently Asked Questions on help path', () => {
-    expect(page.getParagraphTextForPages()).toEqual('Frequently Asked Questions:');
-  });
-
-  it('should display How do I get involved with the FAANG project? on help path', () => {
-    expect(element.all(by.css('h4')).first().getText()).toEqual('How do I get involved with the FAANG project?');
-  });
-
-  it('should display some text when click on arrow on help path', () => {
-    element.all(by.css('summary')).first().click().then(function() {
-      expect(element.all(by.css('details')).first().getText()).toContain('Please register');
-    });
-  });
-});
+// describe('Test help page', () => {
+//   let page: AppPage;
+//
+//   beforeEach(() => {
+//     page = new AppPage();
+//     page.navigateToHelp();
+//   });
+//
+//   it('should display Frequently Asked Questions on help path', () => {
+//     expect(page.getParagraphTextForPages()).toEqual('Frequently Asked Questions:');
+//   });
+//
+//   it('should display How do I get involved with the FAANG project? on help path', () => {
+//     expect(element.all(by.css('h4')).first().getText()).toEqual('How do I get involved with the FAANG project?');
+//   });
+//
+//   it('should display some text when click on arrow on help path', () => {
+//     element.all(by.css('summary')).first().click().then(function() {
+//       expect(element.all(by.css('details')).first().getText()).toContain('Please register');
+//     });
+//   });
+// });
