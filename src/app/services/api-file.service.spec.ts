61547bdf733dc7c04651291445ebad1e092cf983
Alexey Sokolov <alexey@ebi.ac.uk>
2019-04-18 17:00:15 +0100

    Writing test for api-file service

--- src/app/services/api-file.service.spec.ts
+++ src/app/services/api-file.service.spec.ts
@@ -48,4 +48,100 @@ describe('ApiFileService', () => {
       expect(data['hits']['hits'][0]['_source']['name']).toEqual('SRR958466_1.fastq.gz');
     });
   }));
+
+  it('should return experiment for particular file', inject([ApiFileService], (service: ApiFileService) => {
+    service.getFilesExperiment('SRX339479').subscribe(data => {
+      const experiment_should_be = {
+        standardMet: 'Legacy',
+        accession: 'SRX339479',
+        assayType: 'whole genome sequencing assay',
+        experimentTarget: 'input DNA'};
+      expect(data['hits']['hits'][0]['_source']).toEqual(experiment_should_be);
+    });
+  }));
+
+  it('should return first 10 organisms', inject([ApiFileService], (service: ApiFileService) => {
+    const query = {
+      'sort': 'id_number:desc',
+      '_source': [
+        'biosampleId',
+        'sex.text',
+        'organism.text',
+        'breed.text',
+        'standardMet',
+        'id_number',
+        'paperPublished'],
+    };
+    service.getAllOrganisms(query, 10).subscribe(data => {
+      expect(data.length).toEqual(10);
+    });
+  }));
+
+  it('should return detailed information about particular organism', inject([ApiFileService], (service: ApiFileService) => {
+    service.getOrganism('SAMEA104728877').subscribe(data => {
+      expect(data['hits']['hits'][0]['_source']['name']).toEqual('ECA_UCD_AH2');
+    });
+  }));
+
+  it('should return information about specimens of organism', inject([ApiFileService], (service: ApiFileService) => {
+    service.getOrganismsSpecimens('SAMEA104728877').subscribe(data => {
+      expect(data['hits']['hits'][0]['_source']['name']).toEqual('ECA_UCD_S116');
+    });
+  }));
+
+  it('should return first 10 specimens', inject([ApiFileService], (service: ApiFileService) => {
+    const query = {
+      'sort': 'id_number:desc',
+      '_source': [
+        'biosampleId',
+        'material.text',
+        'cellType.text',
+        'organism.sex.text',
+        'organism.organism.text',
+        'organism.breed.text',
+        'standardMet',
+        'id_number',
+        'paperPublished'],
+    };
+    service.getAllSpecimens(query, 10).subscribe(data => {
+      expect(data.length).toEqual(10);
+    });
+  }));
+
+  it('should return detailed information about particular specimen', inject([ApiFileService], (service: ApiFileService) => {
+    service.getSpecimen('SAMEA104728909').subscribe(data => {
+      expect(data['hits']['hits'][0]['_source']['name']).toEqual('ECA_UCD_S63');
+    });
+  }));
+
+  it('should return files information about particular specimen', inject([ApiFileService], (service: ApiFileService) => {
+    service.getSpecimenFiles('SAMEA104728903').subscribe(data => {
+      expect(data['hits']['hits'].length).toEqual(4);
+    });
+  }));
+
+  it('should return first 10 datasets', inject([ApiFileService], (service: ApiFileService) => {
+    const query = {
+      '_source': [
+        'accession',
+        'title',
+        'species.text',
+        'archive',
+        'experiment.accession',
+        'file.name',
+        'specimen.biosampleId',
+        'assayType',
+        'standardMet',
+        'paperPublished'],
+    };
+    service.getAllDatasets(query, 10).subscribe(data => {
+      expect(data.length).toEqual(10);
+    });
+  }));
+
+  it('should return detailed information about particular dataset', inject([ApiFileService], (service: ApiFileService) => {
+    service.getDataset('PRJEB28219').subscribe(data => {
+      expect(data['hits']['hits'][0]['_source']['accession']).toEqual('PRJEB28219');
+    });
+  }));
 });
