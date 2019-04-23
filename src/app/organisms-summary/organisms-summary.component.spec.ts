3756891fcef6a854720c1d939f03f81e5a330630
Alexey Sokolov <alexey@ebi.ac.uk>
2019-04-23 13:54:10 +0100

    Adding unit test for organisms-summary component

--- src/app/organisms-summary/organisms-summary.component.spec.ts
+++ src/app/organisms-summary/organisms-summary.component.spec.ts
@@ -34,4 +34,59 @@ describe('OrganismsSummaryComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create data for charts', () => {
+    const data = {
+      sexSummary: [
+        {
+          name: 'male',
+          value: 1
+        }
+      ],
+      paperPublishedSummary: [
+        {
+          name: 'Yes',
+          value: 1
+        }
+      ],
+      organismSummary: [
+        {
+          name: 'Bos taurus',
+          value: 1
+        }
+      ],
+      standardSummary: [
+        {
+          name: 'FAANG',
+          value: 1
+        }
+      ],
+      breedSummary: [
+        {
+          name: 'Bos taurus',
+          value: [
+            {
+              name: 'Brahman',
+              value: 1
+            }
+          ]
+        }
+      ]
+    };
+    component.assignChartData(data);
+    expect(component.sexChartLabels).toEqual(['male']);
+    expect(component.sexChartData).toEqual([1]);
+
+    expect(component.paperChartLabels).toEqual(['Yes']);
+    expect(component.paperChartData).toEqual([1]);
+
+    expect(component.organismChartLabels).toEqual(['Bos taurus']);
+    expect(component.organismChartData).toEqual([1]);
+
+    expect(component.standardChartLabels).toEqual(['FAANG']);
+    expect(component.standardChartData).toEqual([1]);
+
+    expect(component.breedChartLabels).toEqual(['Brahman']);
+    expect(component.standardChartData).toEqual([1]);
+  });
 });
