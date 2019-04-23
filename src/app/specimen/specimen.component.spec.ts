066d1ad3367e38ffcb69111eff16fe516a1e4438
Alexey Sokolov <alexey@ebi.ac.uk>
2019-04-23 15:38:34 +0100

    Adding unit tests for specimen component

--- src/app/specimen/specimen.component.spec.ts
+++ src/app/specimen/specimen.component.spec.ts
@@ -44,4 +44,93 @@ describe('SpecimenComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('hasActiveFilters should return false if filter_field is empty', () => {
+    expect(component.hasActiveFilters()).toEqual(false);
+  });
+
+  it('hasActiveFilters should return true if filter_field has data', () => {
+    component.filter_field = {
+      standard: ['FAANG']
+    };
+    expect(component.hasActiveFilters()).toEqual(true);
+  });
+
+  it('resetFilter should reset all filters in filter_field', () => {
+    component.filter_field = {
+      standard: ['FAANG']
+    };
+    component.resetFilter();
+    expect(component.filter_field).toEqual({});
+  });
+
+  it('wasPublished should return true if published equals to string with true', () => {
+    expect(component.wasPublished('true')).toEqual(true);
+  });
+
+  it('wasPublished should return false if published equals to string with false', () => {
+    expect(component.wasPublished('false')).toEqual(false);
+  });
+
+  it('isGreen should return green if article was published for this record', () => {
+    expect(component.isGreen('true')).toEqual('green');
+  });
+
+  it('isGreen should return default if article was not published for this record', () => {
+    expect(component.isGreen('false')).toEqual('default');
+  });
+
+  it('selectColumn should assign right value for sort_field', () => {
+    component.selectedColumn = 'BioSample ID';
+    component.selectColumn();
+    expect(component.sort_field['id']).toEqual('idNumber');
+
+    component.selectedColumn = 'Material';
+    component.selectColumn();
+    expect(component.sort_field['id']).toEqual('material');
+
+    component.selectedColumn = 'Organism part/Cell type';
+    component.selectColumn();
+    expect(component.sort_field['id']).toEqual('organismpart_celltype');
+
+    component.selectedColumn = 'Sex';
+    component.selectColumn();
+    expect(component.sort_field['id']).toEqual('sex');
+
+    component.selectedColumn = 'Organism';
+    component.selectColumn();
+    expect(component.sort_field['id']).toEqual('organism');
+
+    component.selectedColumn = 'Breed';
+    component.selectColumn();
+    expect(component.sort_field['id']).toEqual('breed');
+
+    component.selectedColumn = 'Standard';
+    component.selectColumn();
+    expect(component.sort_field['id']).toEqual('standard');
+  });
+
+  it('chooseClass should assign right values for spanClass and sort_field', () => {
+    component.selectedColumn = 'BioSample ID';
+    component.chooseClass('glyphicon glyphicon-arrow-down');
+    expect(component.spanClass).toEqual('glyphicon glyphicon-arrow-up');
+    expect(component.sort_field['direction']).toEqual('asc');
+
+    component.chooseClass('glyphicon glyphicon-arrow-up');
+    expect(component.spanClass).toEqual('glyphicon glyphicon-arrow-down');
+    expect(component.sort_field['direction']).toEqual('desc');
+
+    component.selectedColumn = 'Material';
+    component.chooseClass('glyphicon glyphicon-sort');
+    expect(component.spanClass).toEqual('glyphicon glyphicon-arrow-down');
+    expect(component.sort_field['direction']).toEqual('desc');
+
+    component.chooseClass('glyphicon glyphicon-arrow-down');
+    expect(component.spanClass).toEqual('glyphicon glyphicon-arrow-up');
+    expect(component.sort_field['direction']).toEqual('asc');
+
+    component.chooseClass('glyphicon glyphicon-arrow-up');
+    expect(component.spanClass).toEqual('glyphicon glyphicon-arrow-down');
+    expect(component.sort_field['direction']).toEqual('desc');
+  });
 });
