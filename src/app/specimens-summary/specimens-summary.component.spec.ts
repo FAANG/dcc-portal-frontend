48f2024b9c736b806e9fd4559cb9d912f6c83451
Alexey Sokolov <alexey@ebi.ac.uk>
2019-04-23 14:06:18 +0100

    Adding unit test for specimens-summary component

--- src/app/specimens-summary/specimens-summary.component.spec.ts
+++ src/app/specimens-summary/specimens-summary.component.spec.ts
@@ -34,4 +34,77 @@ describe('SpecimensSummaryComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create data for charts', () => {
+    const data = {
+      sexSummary: [
+        {
+          name: 'male',
+          value: 1
+        }
+      ],
+      paperPublishedSummary: [
+        {
+          name: 'Yes',
+          value: 1
+        }
+      ],
+      standardSummary: [
+        {
+          name: 'FAANG',
+          value: 1
+        }
+      ],
+      cellTypeSummary: [
+        {
+          name: 'blood',
+          value: 1
+        }
+      ],
+      organismSummary: [
+        {
+          name: 'Bos taurus',
+          value: 1
+        }
+      ],
+      materialSummary: [
+        {
+          name: 'specimen from organism',
+          value: 1
+        }
+      ],
+      breedSummary: [
+        {
+          name: 'Bos taurus',
+          value: [
+            {
+              name: 'Brahman',
+              value: 1
+            }
+          ]
+        }
+      ]
+    };
+    component.assignChartData(data);
+    expect(component.sexChartLabels).toEqual(['male']);
+    expect(component.sexChartData).toEqual([1]);
+
+    expect(component.paperChartLabels).toEqual(['Yes']);
+    expect(component.paperChartData).toEqual([1]);
+
+    expect(component.standardChartLabels).toEqual(['FAANG']);
+    expect(component.standardChartData).toEqual([1]);
+
+    expect(component.cellsChartLabels).toEqual(['blood']);
+    expect(component.cellsChartData).toEqual([1]);
+
+    expect(component.organismChartLabels).toEqual(['Bos taurus']);
+    expect(component.organismChartData).toEqual([1]);
+
+    expect(component.materialChartLabels).toEqual(['specimen from organism']);
+    expect(component.materialChartData).toEqual([1]);
+
+    expect(component.breedChartLabels).toEqual(['Brahman']);
+    expect(component.standardChartData).toEqual([1]);
+  });
 });
