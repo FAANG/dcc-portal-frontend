0fd9cd2b591c3ba101d852cd778607676dd106eb
Alexey Sokolov <alexey@ebi.ac.uk>
2019-04-24 12:35:52 +0100

    Adding unit tests for sort pipe

---
+++ src/app/pipes/sort.pipe.spec.ts
@@ -0,0 +1,65 @@
+import {SortPipe} from './sort.pipe';
+
+describe('Pipe: SortPipe', () => {
+  let pipe: SortPipe;
+
+  beforeEach(() => {
+    pipe = new SortPipe();
+  });
+
+  it('should return empty list if value was not provided', () => {
+    const params = {
+      id: 'standard',
+      direction: 'asc'
+    };
+    expect(pipe.transform('', params)).toEqual([]);
+  });
+
+  it('should sort by ascending order', () => {
+    const params = {
+      id: 'idNumber',
+      direction: 'asc'
+    };
+    const value = [
+      {
+        idNumber: 10
+      },
+      {
+        idNumber: 1
+      }
+    ];
+    const to_return = [
+      {
+        idNumber: 1
+      },
+      {
+        idNumber: 10
+      }
+    ];
+    expect(pipe.transform(value, params)).toEqual(to_return);
+  });
+
+  it('should sort by descending order', () => {
+    const params = {
+      id: 'idNumber',
+      direction: 'desc'
+    };
+    const value = [
+      {
+        idNumber: 1
+      },
+      {
+        idNumber: 10
+      }
+    ];
+    const to_return = [
+      {
+        idNumber: 10
+      },
+      {
+        idNumber: 1
+      }
+    ];
+    expect(pipe.transform(value, params)).toEqual(to_return);
+  });
+});
