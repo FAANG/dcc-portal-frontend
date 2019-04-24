169cc601fb908737ba8b17ce8e95bc8f99682ce5
Alexey Sokolov <alexey@ebi.ac.uk>
2019-04-24 10:12:34 +0100

    Adding unit test for protocol-sample component

--- src/app/protocol-sample/protocol-sample.component.spec.ts
+++ src/app/protocol-sample/protocol-sample.component.spec.ts
@@ -42,4 +42,31 @@ describe('ProtocolSampleComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('hasActiveFilters should return false if filter_field is empty', () => {
+    expect(component.hasActiveFilters()).toEqual(false);
+  });
+
+  it('hasActiveFilters should return true if filter_field has data', () => {
+    component.filter_field = {
+      standard: ['FAANG']
+    };
+    expect(component.hasActiveFilters()).toEqual(true);
+  });
+
+  it('resetFilter should reset all filters in filter_field', () => {
+    component.filter_field = {
+      standard: ['FAANG']
+    };
+    component.resetFilter();
+    expect(component.filter_field).toEqual({});
+  });
+
+  it('onDownloadData should change value of downloadData', () => {
+    expect(component.downloadData).toEqual(false);
+    component.onDownloadData();
+    expect(component.downloadData).toEqual(true);
+    component.onDownloadData();
+    expect(component.downloadData).toEqual(false);
+  });
 });
