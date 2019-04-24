37009c34aed12539d9524b35c06a92c367347914
Alexey Sokolov <alexey@ebi.ac.uk>
2019-04-24 12:35:23 +0100

    Adding unit tests for specimen-detail component

--- src/app/specimen/specimen-detail/specimen-detail.component.spec.ts
+++ src/app/specimen/specimen-detail/specimen-detail.component.spec.ts
@@ -38,4 +38,31 @@ describe('SpecimenDetailComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('dealWithAvailability should return email', () => {
+    expect(component.dealWithAvailability('mailto:test@test.com')).toEqual('test@test.com');
+    expect(component.dealWithAvailability('test@test.com')).toEqual('test@test.com');
+  });
+
+  it('getProtocolLink should change ftp on http in link url', () => {
+    component.specimen = {
+      specimenFromOrganism: {
+        specimenCollectionProtocol: {
+          url: 'ftp://test.com'
+        }
+      }
+    };
+    expect(component.getProtocolLink()).toEqual('http://test.com');
+  });
+
+  it('getProtocolLink should not change https in link url', () => {
+    component.specimen = {
+      specimenFromOrganism: {
+        specimenCollectionProtocol: {
+          url: 'http://test.com'
+        }
+      }
+    };
+    expect(component.getProtocolLink()).toEqual('http://test.com');
+  });
 });
