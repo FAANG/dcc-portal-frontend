c234fb8427f690bc71d5b2401d4a513f28c29864
Jun Fan <junf@ebi.ac.uk>
2019-08-19 10:53:47 +0100

    add download checkbox to the shared component using files in analysis as example

--- src/app/shared/related-items/related-items.component.ts
+++ src/app/shared/related-items/related-items.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ApiFileService} from '../../services/api-file.service';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-related-items',
@@ -10,33 +11,50 @@ export class RelatedItemsComponent implements OnInit {
   @Input() record_id: string;
   @Input() source_type: string;
   @Input() target_type: string;
+  @Input() download_key: string;
+
   records: any;
+  urls: string[] = [];
+  checked = true;
+
   p = 1; // page number for html template
   // field names and values must match  i.e. same length and order
   // field names are the table headers
   // field values are the actual value in the table
   // field values having links have value of 'prefix', 'url', 'display'
+  // and keys as source_type-target_entity-display
   field_names = new Map<string, string[]>();
   field_values = new Map<string, string[]>();
   field_values_having_links = new Map<string, string[]>();
 
+
   constructor(private apiFileService: ApiFileService) {
-    this.field_names.set('specimen-specimen', ['specimen-relationship']);
-    this.field_names.set('specimen-file', ['files for specimen']);
-    this.field_names.set('specimen-analysis', ['analyses for specimen']);
+    // files in the analysis detail page
     this.field_names.set('analysis-file', ['Name', 'Type', 'Size', 'Checksum']);
     this.field_values.set('analysis-file', ['name', 'type', 'size', 'checksum']);
-    this.field_values_having_links.set('analysis-file-name', ['ftp://', 'url', 'name']);
+    this.field_values_having_links.set('analysis-file-name', ['ftp://', 'url']);
   }
 
   ngOnInit() {
     const relationship_type = `${this.source_type}-${this.target_type}`;
     if (relationship_type === 'analysis-file') {
       this.apiFileService.getAnalysis(this.record_id).subscribe(
-      (data: any) => {
+        (data: any) => {
           this.records = data['_source']['files'];
-      });
+          const url_location = this.download_key.split('.');
+          if (this.download_key.length > 0) {
+            for (const record of this.records) {
+              let curr: any = record;
+              for (const index of url_location) {
+                curr = curr[index];
+              }
+              this.urls.push('ftp://' + curr);
+            }
+          }
+        });
     }
+
+
   }
 
   get_field_names() {
@@ -54,4 +72,56 @@ export class RelatedItemsComponent implements OnInit {
     return this.field_values_having_links.get(key_value);
   }
 
+  showButton() {
+    return this.target_type === 'file';
+  }
+
+  downloadAllFiles() {
+    this.urls.forEach(url => FileSaver.saveAs(url));
+  }
+
+  onCheckboxClick(url: string) {
+    url = 'ftp://' + url;
+    const index = this.urls.indexOf(url);
+    if (index !== -1) {
+      this.urls.splice(index, 1);
+    } else {
+      this.urls.push(url);
+    }
+    console.log(this.urls.length);
+  }
+
+  disableButton() {
+    return this.urls.length === 0;
+  }
+
+  getUrlsLength() {
+    return this.urls.length;
+  }
+
+  CheckboxChecked(url: string) {
+    url = 'ftp://' + url;
+    return this.urls.indexOf(url) !== -1;
+  }
+
+  mainCheckboxClicked() {
+    if (this.checked === true) {
+      this.urls = [];
+    } else {
+      const url_location = this.download_key.split('.');
+      for (const record of this.records) {
+        let curr: any = record;
+        for (const index of url_location) {
+          curr = curr[index];
+        }
+        const url = 'ftp://' + curr;
+        const idx = this.urls.indexOf(url);
+        if (idx === -1) {
+          this.urls.push(url);
+        }
+      }
+
+    }
+    this.checked = !this.checked;
+  }
 }
