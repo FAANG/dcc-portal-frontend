91bec5f0da03d3d53c76f1362706b750f7b034d9
Alexey Sokolov <alexey@ebi.ac.uk>
2019-08-30 10:19:07 +0100

    Adding anchor scroll

--- src/app/ruleset-sample/ruleset-sample.component.ts
+++ src/app/ruleset-sample/ruleset-sample.component.ts
@@ -1,29 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewChecked, Component, OnInit} from '@angular/core';
 import {Title} from '@angular/platform-browser';
 import {ApiFileService} from '../services/api-file.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-ruleset-sample',
   templateUrl: './ruleset-sample.component.html',
   styleUrls: ['./ruleset-sample.component.css']
 })
-export class RulesetSampleComponent implements OnInit {
+export class RulesetSampleComponent implements OnInit, AfterViewChecked {
   error: string;
   data: any;
+  fragment: string;
 
-  constructor(private titleService: Title, private apiFileService: ApiFileService) { }
+  constructor(private titleService: Title, private apiFileService: ApiFileService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.titleService.setTitle('FAANG Rule set|samples');
     this.apiFileService.getRulesetSample().subscribe(
       data => {
         this.data = data;
-        console.log(data);
       },
       error => {
         this.error = error;
       }
     );
+    this.route.fragment.subscribe(fragment => {
+      this.fragment = fragment;
+    });
+  }
+
+  ngAfterViewChecked(): void {
+    try {
+      if (this.fragment) {
+        document.querySelector('#' + this.fragment).scrollIntoView();
+      }
+    } catch (e) {}
+  }
+
+  removeSpaces(id: string) {
+    return id.split(' ').join('_');
+  }
+
+  checkIsActive(category: string) {
+    return this.removeSpaces(category) === this.fragment;
   }
 
 }
