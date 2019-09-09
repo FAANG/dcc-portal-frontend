f3dd325385a01d365dbc8e4a5984d98384537253
Jun Fan <junf@ebi.ac.uk>
2019-09-09 16:33:25 +0100

    unit test on related-items except the pre-existing codes

--- src/app/shared/related-items/related-items.component.spec.ts
+++ src/app/shared/related-items/related-items.component.spec.ts
@@ -38,7 +38,7 @@ describe('RelatedItemsComponent', () => {
      expect(component).toBeTruthy();
   });
 
-  it('check display status, name should be true and description expected to be false', () => {
+  it('check initial displayed column status in dataset file table', () => {
     // component.selected = new Map();
     // component.selected.set('name', true);
     // component.selected.set('description', false);
@@ -47,4 +47,69 @@ describe('RelatedItemsComponent', () => {
     expect(component.isDisplayed('not existing field name')).toEqual(false);
   });
 
+  it ('check all columns', () => {
+    expect(component.get_all_fields()).toEqual(['File name', 'Experiment', 'Archive', 'File size']);
+  });
+
+  it ('check initial displayed columns', () => {
+    expect(component.get_displayed_fields()).toEqual(['File name', 'Experiment', 'File size']);
+  });
+
+  it ('Get the field name in ES which contains the required attribute value', () => {
+    expect(component.get_attr('Experiment')).toEqual('experiment');
+  });
+
+  it ('Get the field name in ES which contains the link for required attribute', () => {
+    expect(component.get_field_values_for_links('Experiment')).toBeUndefined();
+    const link = component.get_field_values_for_links('File name');
+    expect(link['prefix']).toEqual('../file/');
+    expect(link['url']).toEqual('fileId');
+  });
+
+  it ('file table has download button', () => {
+    expect(component.showButton()).toEqual(true);
+    component.target_type = 'specimen';
+    expect(component.showButton()).toEqual(false);
+  });
+
+  it ('download button disabled when no file selected', () => {
+    // default no file selected
+    expect(component.disableButton()).toEqual(true);
+    // simulate one file selected
+    component.urls.push('http://a.zip');
+    expect(component.disableButton()).toEqual(false);
+  });
+
+  it ('check length of selected file list', () => {
+    // default no file selected
+    expect(component.getUrlsLength()).toEqual(0);
+    // simulate one file selected
+    component.urls.push('http://a.zip');
+    expect(component.getUrlsLength()).toEqual(1);
+  });
+
+  it ('get value from ES structure', () => {
+    const record = {
+      'name': 'value for name',
+      'nested': {
+        'another': {
+          'finally': 'value'
+        }
+      }
+    };
+    expect(component.getValue(record, 'name')).toEqual('value for name');
+    expect(component.getValue(record, 'nested.another.finally')).toEqual('value');
+  });
+
+  it ('capitalize first letter of given string', () => {
+    expect(component.capitalizeFirstLetter('name')).toEqual('Name');
+    expect(component.capitalizeFirstLetter('several words together')).toEqual('Several words together');
+  });
+
+  it ('toggle selected column', () => {
+    const field = 'Archive';
+    expect(component.isDisplayed(field)).toEqual(false);
+    component.toggleSelectedColumn(field);
+    expect(component.isDisplayed(field)).toEqual(true);
+  });
 });
