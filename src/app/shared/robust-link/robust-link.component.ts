15ae74d635a804eb99547b39ab89e8858a42c732
Jun Fan <junf@ebi.ac.uk>
2020-01-02 11:30:17 +0000

    remove legacy styling: adding three dots

--- src/app/shared/robust-link/robust-link.component.ts
+++ src/app/shared/robust-link/robust-link.component.ts
@@ -14,9 +14,6 @@ export class RobustLinkComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    if (this.text && this.text.length === 62) {
-      this.text = this.text + '...';
-    }
   }
 
 }
