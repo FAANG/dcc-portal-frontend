11529fe54d1677b27054bfd061a82b675a6f8246
Alexey Sokolov <alexey@ebi.ac.uk>
2020-10-30 12:03:59 +0000

    Adding file upload component

--- src/app/files-upload/files-upload.component.ts
+++ src/app/files-upload/files-upload.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FileUploader} from 'ng2-file-upload';
-import {validation_service_url} from '../shared/constants';
+import {validation_service_url, validation_ws_url} from '../shared/constants';
 import {makeid} from '../shared/common_functions';
-
-const UploadURL = validation_service_url + '/conversion/samples';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-files-upload',
@@ -11,20 +10,66 @@ const UploadURL = validation_service_url + '/conversion/samples';
   styleUrls: ['./files-upload.component.css']
 })
 export class FilesUploadComponent implements OnInit {
+  UploadURL = validation_service_url + '/protocols_upload/samples';
   fileid = makeid(20);
-  public uploader: FileUploader = new FileUploader({url: UploadURL, itemAlias: this.fileid});
+  public uploader: FileUploader = new FileUploader({url: this.UploadURL, itemAlias: this.fileid});
   public link: string;
+  socket;
+  submission_message: string;
+  submission_results: string;
+  errors;
 
-  constructor() { }
+  constructor(private titleService: Title) { }
 
   ngOnInit() {
+    this.titleService.setTitle('FAANG upload protocols');
+    this.setSocket();
     this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
     this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
       console.log(response);
-      setTimeout(() => {
-        this.link = 'https://data.faang.org/api/fire_api/samples/ROSLIN_SOP_GENESWITCH_E15_EMBRYO_SAMPLING_20200915.pdf';
-      }, 3000);
     };
+  }
+
+  setSocket() {
+    const url = validation_ws_url + this.fileid + '/';
+    this.socket = new WebSocket(url);
+    this.socket.onopen = () => {
+      console.log('WebSockets connection created.');
+    };
+    this.socket.onmessage = (event) => {
+      const data = JSON.parse(event.data)['response'];
+      if (data['submission_message']) {
+        this.submission_message = data['submission_message'];
+      }
+      if (data['errors']) {
+        this.errors = data['errors'];
+      }
+      if (data['submission_results']) {
+        this.submission_results = data['submission_results'];
+      }
+    };
+  }
+
+  chooseProtocolType(protocolType: string) {
+    this.UploadURL = `${validation_service_url}/protocols_upload/${protocolType}`;
+    this.uploader = new FileUploader({url: this.UploadURL, itemAlias: this.fileid});
+    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+    this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+      console.log(response);
+    };
+    console.log(this.UploadURL);
+  }
+
+  getAlertClass() {
+    if (this.submission_message) {
+      if (this.submission_message === 'Starting to validate protocol') {
+        return 'alert alert-info';
+      } else if (this.submission_message === 'Protocol upload failed') {
+        return 'alert alert-danger';
+      } else {
+        return 'alert alert-success';
+      }
+    }
   }
 
 }
