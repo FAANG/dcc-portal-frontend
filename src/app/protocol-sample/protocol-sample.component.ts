de893c8c0d905c10b879c898321c29e75b46c3ba
Akshatha nayak <akshatha@ebi.ac.uk>
2020-12-03 20:43:04 +0530

    replace protocol-samples table

--- src/app/protocol-sample/protocol-sample.component.ts
+++ src/app/protocol-sample/protocol-sample.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, AfterViewInit, TemplateRef, ViewChild, ViewChildren, QueryList} from '@angular/core';
 import {Observable} from 'rxjs/internal/Observable';
 import {Subscription} from 'rxjs/internal/Subscription';
 import {ApiDataService} from '../services/api-data.service';
@@ -7,19 +7,25 @@ import {AggregationService} from '../services/aggregation.service';
 import {ExportService} from '../services/export.service';
 import {NgxSpinnerService} from 'ngx-spinner';
 import {Title} from '@angular/platform-browser';
+import {TableClientSideComponent}  from '../shared/table-client-side/table-client-side.component';
 
 @Component({
   selector: 'app-protocol-sample',
   templateUrl: './protocol-sample.component.html',
   styleUrls: ['./protocol-sample.component.css']
 })
-export class ProtocolSampleComponent implements OnInit, OnDestroy {
+export class ProtocolSampleComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('protocolNameTemplate', { static: true }) protocolNameTemplate: TemplateRef<any>;
+  @ViewChildren("tableComp") tableComponents: QueryList<TableClientSideComponent>;
+  private tableClientComponent: TableClientSideComponent;
   protocolList: Observable<any[]>;
   protocolListSubscription: Subscription;
   aggrSubscription: Subscription;
   exportSubscription: Subscription;
   columnNames: string[] = ['Protocol name', 'Organisation', 'Year of protocol'];
   exportNames: string[] = ['Organisation', 'Protocol Year', 'Protocol name'];
+  displayFields: string[] = ['protocol_name', 'university_name', 'protocol_date'];
+  templates: Object;
   filter_field: {};
   downloadData = false;
 
@@ -27,8 +33,6 @@ export class ProtocolSampleComponent implements OnInit, OnDestroy {
   optionsTabular;
   data = {};
 
-  // Local variable for pagination
-  p = 1;
   error: string;
 
   constructor(private dataService: ApiDataService,
@@ -40,6 +44,7 @@ export class ProtocolSampleComponent implements OnInit, OnDestroy {
               private titleService: Title) { }
 
   ngOnInit() {
+    this.templates = {'protocol_name': this.protocolNameTemplate};
     this.titleService.setTitle('FAANG protocols');
     this.spinner.show();
     this.activatedRoute.queryParams.subscribe((params: Params) => {
@@ -60,6 +65,7 @@ export class ProtocolSampleComponent implements OnInit, OnDestroy {
       }
       this.aggregationService.field.next(this.aggregationService.active_filters);
       this.filter_field = filters;
+      this.filter_field = Object.assign({}, this.filter_field);
     });
     this.optionsCsv = this.exportService.optionsCsv;
     this.optionsTabular = this.exportService.optionsTabular;
@@ -81,6 +87,14 @@ export class ProtocolSampleComponent implements OnInit, OnDestroy {
     });
     this.exportSubscription = this.exportService.data.subscribe((data) => {
       this.data = data;
+    });
+  }
+
+  ngAfterViewInit() {
+    this.tableComponents.changes.subscribe((comps: QueryList <TableClientSideComponent>) => {
+        this.tableClientComponent = comps.first;
+        this.aggregationService.getAggregations(this.tableClientComponent.dataSource.filteredData, 'protocol');
+        this.data = this.tableClientComponent.dataSource.sortData(this.tableClientComponent.dataSource.filteredData,this.tableClientComponent.dataSource.sort);
     });
   }
 
