0c35ef9ecbbc05ed78b2a7a3e87ad6693e888fc5
Akshatha nayak <akshatha@ebi.ac.uk>
2020-12-03 17:33:40 +0530

    replaces Analyses table

--- src/app/analysis/analysis.component.ts
+++ src/app/analysis/analysis.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {AnalysisTable, SortParams} from '../shared/interfaces';
+import {Component, OnDestroy, OnInit, AfterViewInit, TemplateRef, ViewChild, ViewChildren, QueryList} from '@angular/core';
+import {AnalysisTable} from '../shared/interfaces';
 import {Subscription} from 'rxjs';
 import {ApiDataService} from '../services/api-data.service';
 import {AggregationService} from '../services/aggregation.service';
@@ -8,21 +8,24 @@ import {NgxSpinnerService} from 'ngx-spinner';
 import {Title} from '@angular/platform-browser';
 import {Observable} from 'rxjs/internal/Observable';
 import {ActivatedRoute, Params, Router} from '@angular/router';
+import {TableClientSideComponent}  from '../shared/table-client-side/table-client-side.component';
 
 @Component({
   selector: 'app-analysis',
   templateUrl: './analysis.component.html',
   styleUrls: ['./analysis.component.css']
 })
-export class AnalysisComponent implements OnInit, OnDestroy {
+export class AnalysisComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('accessionTemplate', { static: true }) accessionTemplate: TemplateRef<any>;
+  @ViewChildren("tableComp") tableComponents: QueryList<TableClientSideComponent>;
+  private tableClientComponent: TableClientSideComponent;
+
   analysisListShort: Observable<AnalysisTable[]>;
   analysisListLong: Observable<AnalysisTable[]>;
   columnNames: string[] = ['Analysis accession', 'Dataset', 'Title', 'Species', 'Assay type', 'Analysis type', 'Standard'];
+  displayFields: string[] = ['accession', 'datasetAccession', 'title', 'species', 'assayType', 'analysisType', 'standard'];
+  templates: Object;
 
-  spanClass = 'expand_more';
-  defaultClass = 'unfold_more';
-  selectedColumn = 'Analysis accession';
-  sort_field: SortParams;
   filter_field = {};
   aggrSubscription: Subscription;
   exportSubscription: Subscription;
@@ -32,9 +35,6 @@ export class AnalysisComponent implements OnInit, OnDestroy {
   optionsCsv;
   optionsTabular;
   data = {};
-
-  // Local variable for pagination
-  p = 1;
 
   private query = {
     'sort': 'accession:desc',
@@ -59,6 +59,7 @@ export class AnalysisComponent implements OnInit, OnDestroy {
               private titleService: Title) { }
 
   ngOnInit() {
+    this.templates = {'accession': this.accessionTemplate};
     this.titleService.setTitle('FAANG analyses');
     this.spinner.show();
     this.activatedRoute.queryParams.subscribe((params: Params) => {
@@ -79,12 +80,11 @@ export class AnalysisComponent implements OnInit, OnDestroy {
       }
       this.aggregationService.field.next(this.aggregationService.active_filters);
       this.filter_field = filters;
+      this.filter_field = Object.assign({}, this.filter_field);
     });
     this.optionsCsv = this.exportService.optionsCsv;
     this.optionsTabular = this.exportService.optionsTabular;
     this.optionsCsv['headers'] = this.columnNames;
-    this.optionsTabular['headers'] = this.optionsTabular;
-    this.sort_field = {id: 'accession', direction: 'desc'};
     this.spinner.hide();
     this.analysisListLong = this.dataService.getAllAnalyses(this.query, 100000);
     this.analysisListLongSubscription = this.analysisListLong.subscribe((data) => {
@@ -104,71 +104,12 @@ export class AnalysisComponent implements OnInit, OnDestroy {
     });
   }
 
-  onTableClick(event: any) {
-    let event_class;
-    if (event['srcElement']['firstElementChild']) {
-      event_class = event['srcElement']['firstElementChild']['innerText'];
-    } else {
-      event_class = event['srcElement']['innerText'];
-    }
-    this.selectedColumn = event['srcElement']['id'];
-    this.selectColumn();
-    this.chooseClass(event_class);
-  }
-
-  chooseClass(event_class: string) {
-    if (this.selectedColumn === 'Analysis accession') {
-      if (event_class === 'expand_more') {
-        this.spanClass = 'expand_less';
-        this.sort_field['direction'] = 'asc';
-      } else {
-        this.spanClass = 'expand_more';
-        this.sort_field['direction'] = 'desc';
-      }
-    } else {
-      if (event_class === this.defaultClass) {
-        this.spanClass = 'expand_more';
-        this.sort_field['direction'] = 'desc';
-      } else if (event_class === 'expand_more') {
-        this.spanClass = 'expand_less';
-        this.sort_field['direction'] = 'asc';
-      } else {
-        this.spanClass = 'unfold_more';
-        this.sort_field['direction'] = 'desc';
-        this.sort_field['id'] = 'datasetAccession';
-        this.selectedColumn = 'Analysis accession';
-        this.spanClass = 'expand_more';
-      }
-    }
-  }
-
-  selectColumn() {
-    switch (this.selectedColumn) {
-      case 'Dataset': {
-        this.sort_field['id'] = 'datasetAccession';
-        break;
-      }
-      case 'Title': {
-        this.sort_field['id'] = 'title';
-        break;
-      }
-      case 'Species': {
-        this.sort_field['id'] = 'species';
-        break;
-      }
-      case 'Analysis accession': {
-        this.sort_field['id'] = 'accession';
-        break;
-      }
-      case 'Assay type': {
-        this.sort_field['id'] = 'assayType';
-        break;
-      }
-      case 'Analysis type': {
-        this.sort_field['id'] = 'analysisType';
-        break;
-      }
-    }
+  ngAfterViewInit() {
+    this.tableComponents.changes.subscribe((comps: QueryList <TableClientSideComponent>) => {
+        this.tableClientComponent = comps.first;
+        this.aggregationService.getAggregations(this.tableClientComponent.dataSource.filteredData, 'analysis');
+        this.data = this.tableClientComponent.dataSource.sortData(this.tableClientComponent.dataSource.filteredData,this.tableClientComponent.dataSource.sort);
+    });
   }
 
   hasActiveFilters() {
