e1a4bc915db71dba920024e48d76b77c8d69341d
yroochun <yroochun@ebi.ac.uk>
2021-12-07 17:28:54 +0000

    abc-94

--- src/app/subprojects/subproject.component.ts
+++ src/app/subprojects/subproject.component.ts
@@ -9,6 +9,7 @@ import setting from './subproject-detail/subproject-detail.component.setting.jso
 })
 export class SubprojectComponent implements OnInit {
   projects = [];
+  eurofaang_proj: any;
 
   constructor(private title: Title) { }
 
@@ -17,20 +18,33 @@ export class SubprojectComponent implements OnInit {
     let idx = 0;
     let isStart = true;
     const tmp = [];
+
     for (const [key, value] of Object.entries(setting)) {
       const obj = value;
+      console.log(obj);
+
       obj['id'] = key;
-      obj['isStart'] = isStart;
-      tmp[idx] = obj;
-      idx++;
-      isStart = !isStart;
+      if (key !== 'EuroFAANG') {
+
+        obj['isStart'] = isStart;
+        tmp[idx] = obj;
+        idx++;
+        isStart = !isStart;
+      } else {
+        // deal with eurofaang separately
+        this.eurofaang_proj = obj;
+        console.log(obj);
+      }
+
+
     }
+
     const pair_num = Math.ceil(idx / 2);
     for (let i = 0; i < pair_num; i++) {
       const obj = {'left': tmp[i * 2], 'right': tmp[2 * i + 1]};
       this.projects[i] = obj;
     }
-    console.log(this.projects);
+    // console.log(this.projects);
   }
 
 }
