cea1f274c3950994d86a74bf3f38102f6e734598
Akshatha nayak <akshatha@ebi.ac.uk>
2022-05-24 11:43:01 +0100

    upload protocols to local storage

--- src/app/files-upload/files-upload.component.ts
+++ src/app/files-upload/files-upload.component.ts
@@ -10,67 +10,18 @@ import {Title} from '@angular/platform-browser';
   styleUrls: ['./files-upload.component.css']
 })
 export class FilesUploadComponent implements OnInit {
-  UploadURL = 'https://data.faang.org/validation_api' + '/protocols_upload/samples';
-  fileid = makeid(20);
-  public uploader: FileUploader = new FileUploader({url: this.UploadURL, itemAlias: this.fileid});
-  public link: string;
-  socket;
-  submission_message: string;
-  submission_results: string;
+  UploadURL;
   errors;
 
   constructor(private titleService: Title) { }
 
   ngOnInit() {
     this.titleService.setTitle('FAANG upload protocols');
-    this.setSocket();
-    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
-    this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
-      console.log(response);
-    };
-  }
-
-  setSocket() {
-    const url = 'wss://data.faang.org/validation_ws/ws/submission/' + this.fileid + '/';
-    this.socket = new WebSocket(url);
-    this.socket.onopen = () => {
-      console.log('WebSockets connection created.');
-    };
-    this.socket.onmessage = (event) => {
-      const data = JSON.parse(event.data)['response'];
-      if (data['submission_message']) {
-        this.submission_message = data['submission_message'];
-      }
-      if (data['errors']) {
-        this.errors = data['errors'];
-      }
-      if (data['submission_results']) {
-        this.submission_results = data['submission_results'];
-      }
-    };
+    this.UploadURL = `${validation_service_url}/protocols_upload/samples`;
   }
 
   chooseProtocolType(protocolType: string) {
-    this.UploadURL = `https://data.faang.org/validation_api/protocols_upload/${protocolType}`;
-    this.uploader = new FileUploader({url: this.UploadURL, itemAlias: this.fileid});
-    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
-    this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
-      console.log(response);
-    };
-    console.log(this.UploadURL);
-  }
-
-  getAlertClass() {
-    if (this.submission_message) {
-      if (this.submission_message === 'Starting to validate protocol' ||
-        this.submission_message === 'Uploading protocol') {
-        return 'alert alert-info';
-      } else if (this.submission_message === 'Protocol upload failed, please contact faang-dcc@ebi.ac.uk') {
-        return 'alert alert-danger';
-      } else {
-        return 'alert alert-success';
-      }
-    }
+    this.UploadURL = `${validation_service_url}/protocols_upload/${protocolType}`;
   }
 
 }
