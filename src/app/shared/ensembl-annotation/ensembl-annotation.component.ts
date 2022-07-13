bbcaf0c7abdd8e05616114ec5a4ee3ff2f3345b5
Yogmatee Roochun <yroochun@ebi.ac.uk>
2022-07-13 11:12:45 +0100

    abc-156: updated mat-table cells to use recursion

--- src/app/shared/ensembl-annotation/ensembl-annotation.component.ts
+++ src/app/shared/ensembl-annotation/ensembl-annotation.component.ts
@@ -1,10 +1,9 @@
-import {Component, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {ApiDataService} from '../../services/api-data.service';
 import {UserService} from '../../services/user.service';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
-import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-ensembl-annotation',
@@ -20,11 +19,90 @@ export class EnsemblAnnotationComponent implements OnInit {
   totalHits = 0;
   displayedColumns: string[] = ['species', 'accession', 'assembly_submitter', 'annotation', 'proteins',
     'transcripts', 'softmasked_genome', 'repeat_library', 'other_data', 'browser_view'];
+  tableDefinitions = [
+    {
+      columnDefinition: 'species',
+      columnTitle: 'Species',
+      cellVar: 'species'
+    },
+    {
+      columnDefinition: 'accession',
+      columnTitle: 'Accession',
+      cellVar: 'accession'
+    },
+    {
+      columnDefinition: 'assembly_submitter',
+      columnTitle: 'Assembly submitted by',
+      cellVar: 'assembly_submitter'
+    },
+    {
+      columnDefinition: 'annotation',
+      columnTitle: 'Annotation',
+      cellVar: 'annotation',
+      span: {
+        href: 'fileUrl',
+        linkText: 'annotation'
+      }
+    },
+    {
+      columnDefinition: 'proteins',
+      columnTitle: 'Proteins',
+      cellVar: 'proteins',
+      span: {
+        href: 'fileUrl',
+        linkText: 'fileType'
+      }
+    },
+    {
+      columnDefinition: 'transcripts',
+      columnTitle: 'Transcripts',
+      cellVar: 'transcripts',
+      span: {
+        href: 'fileUrl',
+        linkText: 'fileType'
+      }
+    },
+    {
+      columnDefinition: 'softmasked_genome',
+      columnTitle: 'Softmasked genome',
+      cellVar: 'softmasked_genome',
+      span: {
+        href: 'fileUrl',
+        linkText: 'fileType'
+      }
+    },
+    {
+      columnDefinition: 'repeat_library',
+      columnTitle: 'Repeat library',
+      cellVar: 'repeat_library',
+      span: {
+        href: 'fileUrl',
+        linkText: 'library'
+      }
+    },
+    {
+      columnDefinition: 'other_data',
+      columnTitle: 'Other data',
+      cellVar: 'other_data',
+      span: {
+        href: 'fileUrl',
+        linkText: 'otherData'
+      }
+    },
+    {
+      columnDefinition: 'browser_view',
+      columnTitle: 'View in browser',
+      cellVar: 'browser_view',
+      span: {
+        href: 'fileUrl',
+        linkText: 'browserView'
+      }
+    }
+  ];
 
   constructor(
     private dataService: ApiDataService,
-    private _userService: UserService,
-    private http: HttpClient) {
+    private _userService: UserService) {
   }
 
   ngOnInit() {
