d3b7a16d1f0ec0630a1c9892e72a0b5a8b495c97
Akshatha nayak <akshatha@ebi.ac.uk>
2022-07-26 11:21:34 +0100

    testing backend fallback

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   cookieDomain: 'data.faang.org',
   envName: 'prod',
-  host: 'https://api.faang.org/',
+  host: 'http://34.79.255.21:8000/',
   relatedProjectsHost: 'https://raw.githubusercontent.com/FAANG/comm-data-portal-projects/master/projects/'
 };
