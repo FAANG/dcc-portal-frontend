bd01e9a31e45299e2d318ac7798f5a4334be631a
Akshatha nayak <akshatha@ebi.ac.uk>
2022-11-10 16:00:04 +0000

    configuring genome

--- src/app/local-genome-browser/local-genome-browser.component.ts
+++ src/app/local-genome-browser/local-genome-browser.component.ts
@@ -9,27 +9,53 @@ import * as igv from 'igv'
 export class LocalGenomeBrowserComponent implements OnInit, AfterViewInit {
   @ViewChild('igvdiv') igvDiv: ElementRef;
   tracks;
-   browser: any;
-   options = {
-      genome: "susScr11"
-   };
+  browser: any;
+  options: {};
 
   constructor() { }
 
   ngOnInit(): void {
+    // TO DO: set options and tracks data dynamically
+    // use "reference" option for displaying ensembl genes
+    // "reference" is not required when using default config with refseq genes
+    this.options = {
+        genome: "susScr11",
+        reference: {
+          "id": "susScr11",
+          "fastaURL": "https://s3.amazonaws.com/igv.org.genomes/susScr11/susScr11.fa",
+          "tracks": [
+            // {
+            //   "name": "Ensembl Genes",
+            //   "url": "https://ftp.ensembl.org/pub/release-108/gff3/sus_scrofa/Sus_scrofa.Sscrofa11.1.108.gff3.gz",
+            //   "order": 1000000,
+            //   "indexed": false
+            // },
+            {
+              "name": "RefSeq Genes",
+              "url": "https://s3.amazonaws.com/igv.org.genomes/susScr11/refGene.sorted.txt.gz",
+              "order": 1000000,
+              "indexed": false
+            }
+          ]
+        }
+     };
     this.tracks = [
       {
         'longLabel': 'liver_fetus_control',
-        'bigDataUrl': 'https://api.faang.org/files/trackhubs/GENE-SwitCH_WP5_ATAC-seq/Sscrofa11.1/gs-wp5/liver_fetus_control.mRp.clN.bigWig'
+        'bigDataUrl': 'https://api.faang.org/files/trackhubs/GENE-SwitCH_WP5_ATAC-seq/Sscrofa11.1/gs-wp5/liver_fetus_control.mRp.clN.bigWig',
       },
       {
         'longLabel': 'liver_fetus_highfibre',
-        'bigDataUrl': 'https://api.faang.org/files/trackhubs/GENE-SwitCH_WP5_ATAC-seq/Sscrofa11.1/gs-wp5/liver_fetus_highfibre.mRp.clN.bigWig'
+        'bigDataUrl': 'https://api.faang.org/files/trackhubs/GENE-SwitCH_WP5_ATAC-seq/Sscrofa11.1/gs-wp5/liver_fetus_highfibre.mRp.clN.bigWig',
       },
       {
         'longLabel': 'liver_fetus_lowfibre',
-        'bigDataUrl': 'https://api.faang.org/files/trackhubs/GENE-SwitCH_WP5_ATAC-seq/Sscrofa11.1/gs-wp5/liver_fetus_lowfibre.mRp.clN.bigWig'
+        'bigDataUrl': 'https://api.faang.org/files/trackhubs/GENE-SwitCH_WP5_ATAC-seq/Sscrofa11.1/gs-wp5/liver_fetus_lowfibre.mRp.clN.bigWig',
       },
+      {
+        'longLabel': 'consensus_peak',
+        'bigDataUrl': 'https://api.faang.org/files/trackhubs/GENE-SwitCH_WP5_ATAC-seq/Sscrofa11.1/gs-wp5/consensus_peaks.bb',
+      }
     ];
   }
 
