2db55daab7efd9e2af5be824aeb216272b0b456f
Alexey Sokolov <alexey@ebi.ac.uk>
2022-12-02 09:29:21 +0000

    Changing BE endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   cookieDomain: 'localhost',
-  host: 'http://45.88.81.195:8000/',
+  host: 'https://api.faang.org/',
   relatedProjectsHost: 'https://raw.githubusercontent.com/FAANG/comm-data-portal-projects/master/projects/'
 };
 
