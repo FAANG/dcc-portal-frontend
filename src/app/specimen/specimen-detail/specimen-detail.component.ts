959ba6b6fe03f8fefbe7ff918114f843940eff0e
Alexey Sokolov <alexey@ebi.ac.uk>
2023-09-14 12:38:44 +0100

    Fixing typos in organoids field names

--- src/app/specimen/specimen-detail/specimen-detail.component.ts
+++ src/app/specimen/specimen-detail/specimen-detail.component.ts
@@ -74,4 +74,10 @@ export class SpecimenDetailComponent implements OnInit {
   generateProtocolLink(url: string) {
     return getProtocolLink(url);
   }
+
+  isOrganoid(biosampleid: string) {
+    return ['SAMEA113988823', 'SAMEA113988824', 'SAMEA113988825', 'SAMEA113988826', 'SAMEA113988827', 'SAMEA113988828', 'SAMEA113988829',
+      'SAMEA113988830', 'SAMEA113988831', 'SAMEA113988832',  'SAMEA113988833', 'SAMEA113988834', 'SAMEA113988835', 'SAMEA113988836',
+      'SAMEA113988837',  'SAMEA113988838'].indexOf(biosampleid) !== -1;
+  }
 }
