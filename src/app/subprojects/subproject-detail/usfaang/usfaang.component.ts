7d328d6df5ac692ca2b028e0f6e07dcaec47396f
Yogmatee Roochun <yroochun@ebi.ac.uk>
2024-03-01 16:35:17 +0000

    added usfaang project

--- src/app/subprojects/subproject-detail/usfaang/usfaang.component.ts
+++ src/app/subprojects/subproject-detail/usfaang/usfaang.component.ts
@@ -1,6 +1,7 @@
 import {Component, ViewChild, AfterViewInit} from '@angular/core';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
+
 export interface DatasetElement {
   accession: string;
   title: string;
@@ -26,29 +27,24 @@ export class UsfaangComponent implements AfterViewInit {
       species: 'Sus scrofa', archive: 'ENA', assayType: 'snATAC-seq', experimentsNumber: '1',
       specimensNumber: '1', filesNumber: '8', standard: 'FAANG'
     },
-
     {
       accession: 'PRJEB68307',
       title: 'Chromatin accessibility landscape of unstimulated porcine circulating immune cells at single cell resolution',
       species: 'Sus scrofa', archive: 'ENA', assayType: 'snATAC-seq', experimentsNumber: '1',
       specimensNumber: '1', filesNumber: '8', standard: 'FAANG'
     },
-
     {
       accession: 'PRJEB57595',
       title: 'Transcriptome profiling of porcine neutrophils',
       species: 'Sus scrofa', archive: 'ENA', assayType: 'transcription profiling by high throughput sequencing', experimentsNumber: '2',
       specimensNumber: '2', filesNumber: '4', standard: 'FAANG'
     },
-
     {
       accession: 'PRJEB51699',
       title: 'Characteriziation of regulatory elements in sorted porcine immune cells',
       species: 'Sus scrofa', archive: 'ENA', assayType: 'ChIP-seq,ATAC-seq', experimentsNumber: '105',
       specimensNumber: '15', filesNumber: '924', standard: 'FAANG'
     },
-
-
     {
       accession: 'PRJEB49412',
       title: 'Characteriziation of allele-biased expression and methylation events in porcine fetal tissues',
@@ -60,57 +56,48 @@ export class UsfaangComponent implements AfterViewInit {
       filesNumber: '1102',
       standard: 'FAANG'
     },
-
     {
       accession: 'PRJEB47517',
       title: 'Assessment of DNA methylation in porcine immune cells',
       species: 'Sus scrofa', archive: 'ENA', assayType: 'methylation profiling by high throughput sequencing', experimentsNumber: '41',
       specimensNumber: '15', filesNumber: '82', standard: 'FAANG'
     },
-
     {
       accession: 'PRJEB43826',
       title: 'Transcriptional landscape of porcine circulating immune cells',
       species: 'Sus scrofa', archive: 'ENA', assayType: 'transcription profiling by high throughput sequencing', experimentsNumber: '22',
       specimensNumber: '22', filesNumber: '44', standard: 'FAANG'
     },
-
     {
       accession: 'PRJEB37735',
       title: 'Identification of genome-wide regulatory elements of gut tissues in livestock species',
       species: 'Sus scrofa', archive: 'ENA', assayType: 'ChIP-seq,RNA-seq of total RNA', experimentsNumber: '77',
       specimensNumber: '13', filesNumber: '89', standard: 'FAANG'
     },
-
     {
       accession: 'PRJEB37103',
       title: 'Pig fetal histone modification altas',
       species: 'Sus scrofa', archive: 'ENA', assayType: 'ChIP-seq', experimentsNumber: '20',
       specimensNumber: '4', filesNumber: '40', standard: 'FAANG'
     },
-
     {
       accession: 'PRJEB31483',
       title: 'Pig histone marks',
       species: 'Sus scrofa', archive: 'ENA', assayType: 'ChIP-seq', experimentsNumber: '72',
       specimensNumber: '24', filesNumber: '144', standard: 'FAANG'
     },
-
     {
       accession: 'PRJEB31482',
       title: 'Pig histone modification alias',
       species: 'Sus scrofa', archive: 'ENA', assayType: 'ChIP-seq', experimentsNumber: '20',
       specimensNumber: '4', filesNumber: '40', standard: 'FAANG'
     },
-
     {
       accession: 'PRJEB31481',
       title: 'RNA-seq of pig alveolar macrophages stimulated with LPS and Poly (I:C)',
       species: 'Sus scrofa', archive: 'ENA', assayType: 'transcription profiling by high throughput sequencing', experimentsNumber: '24',
       specimensNumber: '24', filesNumber: '48', standard: 'FAANG'
-    },
-
-
+    }
   ];
   displayedColumns: string[] = ['accession', 'title', 'species', 'archive', 'assayType', 'experimentsNumber',
     'specimensNumber', 'filesNumber', 'standard'];
