2ebf6eebb693dbe50cd948b2448816f2a45b6c62
Yogmatee Roochun <yroochun@ebi.ac.uk>
2024-07-16 16:32:02 +0100

    angular 18 updates - updated header flex

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,7 +3,9 @@ import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { NgClass } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
 import { MatMenuTrigger, MatMenu, MatMenuItem } from '@angular/material/menu';
+import { ExtendedModule } from '@angular/flex-layout/extended';
 import { MatButton } from '@angular/material/button';
+import { FlexModule } from '@angular/flex-layout/flex';
 import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
@@ -11,7 +13,8 @@ import { MatToolbar } from '@angular/material/toolbar';
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css'],
     standalone: true,
-    imports: [MatToolbar, RouterLink, MatButton, MatMenuTrigger, MatIcon, RouterLinkActive, NgClass, MatMenu, MatMenuItem]
+    imports: [MatToolbar, FlexModule, RouterLink, MatButton, ExtendedModule, MatMenuTrigger, MatIcon, RouterLinkActive, NgClass,
+      MatMenu, MatMenuItem]
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
